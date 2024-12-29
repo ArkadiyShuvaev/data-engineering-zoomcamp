2eb1da3d4c2e997f1a85b66f77c7c2b8a1ed4828
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2024-12-29 22:27:57 +0100

    temp commit to save work

---
+++ scripts/init-db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "ny_taxi";
+    CREATE DATABASE "rag_application";
+EOSQL
