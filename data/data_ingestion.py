4637bf0757e6023259da420fbbeb4fccb5efb464
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2025-01-07 01:55:10 +0000

    first implementation of the ingestion script

---
+++ data/data_ingestion.py
@@ -0,0 +1,72 @@
+import argparse
+import subprocess
+import sys
+from time import time
+from urllib.parse import quote_plus
+
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from sqlalchemy import create_engine
+
+
+def main(params) -> int:
+    password = params.db_password
+    host_name = params.db_host_name
+    db_name = params.db_name
+    db_port = params.db_port
+    table_name = params.db_table_name
+    file_url = params.file_url
+    user_name = params.db_user_name
+
+    chunk_size = 100000
+    downloaded_file_path = "/var/tmp/output.parquet"
+
+    subprocess.run(["wget", "-O", downloaded_file_path, file_url], check=True)
+
+    encoded_password = quote_plus(password)
+    engine = create_engine(
+        f"postgresql://{user_name}:{encoded_password}@{host_name}:{db_port}/{db_name}")
+
+    print("Loading data into PostgreSQL")
+    parquet_file = pq.ParquetFile(downloaded_file_path)
+
+    is_first_batch = True
+    for batch in parquet_file.iter_batches(batch_size=chunk_size):
+        start = time()
+        chunk_df = pa.Table.from_batches([batch]).to_pandas()
+
+        if is_first_batch:
+            chunk_df.to_sql(table_name, engine, index=False, if_exists="replace")
+            is_first_batch = False
+        else:
+            chunk_df.to_sql(table_name, engine, index=False, if_exists="append")
+
+        end = time()
+        print(f"Chunk loaded in {end - start} seconds")
+
+    row_count = pd.read_sql(f"SELECT COUNT(*) FROM {table_name}", engine)
+    print(f"Number of rows in the table: {row_count.iloc[0, 0]}")
+
+    return 0
+
+
+if __name__ == '__main__':
+    print("Starting Data Ingestion")
+
+    parser = argparse.ArgumentParser("Ingest Data File into PostgreSQL")
+    parser.add_argument("--file_url", help="URL of the data to be downloaded")
+    parser.add_argument("--db_table_name", help="The name of the table to be created")
+    parser.add_argument("--db_name", help="The name of the database to be created")
+    parser.add_argument("--db_host_name", help="The name of the host")
+    parser.add_argument("--db_port", help="The port number of the database")
+    parser.add_argument("--db_password", help="The password of the user")
+    parser.add_argument("--db_user_name", help="The username to connect to the database")
+
+    args = parser.parse_args()
+
+    try:
+        sys.exit(main(args))
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 A data engineering course.
 
 <img src="./img/cbb7c6dd-7f8a-4565-b2fd-96abf31632d0.webp" alt="course picture" height="300">
+
+## Course Description
+
+### How to test the data ingestion script
+To ingest data from a source and load it into a target, you can use the following command:
+```bash
+python ./data/data_ingestion.py --file_url https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2019-09.parquet --db_table_name green_tripdata_2019_09 --db_name ny_taxi --db_host_name db --db_port 5432 --db_user_name postgres --db_password P@ssw0rd!
+```
+
+### How to build an image to ingest data from a source and load it into a target
