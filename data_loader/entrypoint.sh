bc681e99218ccc1e56cf4e02cdbd634924118285
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2025-01-10 18:25:07 +0000

    added script to load data to the datatable using Docker

---
+++ data_loader/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Check if all required environment variables are provided
+if [ -z "$FILE_URL" ]; then
+  echo "FILE_URL is not set"
+  exit 1
+fi
+
+if [ -z "$DB_TABLE_NAME" ]; then
+  echo "DB_TABLE_NAME is not set"
+  exit 1
+fi
+
+if [ -z "$DB_NAME" ]; then
+  echo "DB_NAME is not set"
+  exit 1
+fi
+
+if [ -z "$DB_HOST_NAME" ]; then
+  echo "DB_HOST_NAME is not set"
+  exit 1
+fi
+
+if [ -z "$DB_PORT" ]; then
+  echo "DB_PORT is not set"
+  exit 1
+fi
+
+if [ -z "$DB_USER_NAME" ]; then
+  echo "DB_USER_NAME is not set"
+  exit 1
+fi
+
+if [ -z "$DB_PASSWORD" ]; then
+  echo "DB_PASSWORD is not set"
+  exit 1
+fi
+
+# Execute the data_loader.py script with the provided environment variables
+python data_loader.py --file_url "$FILE_URL" --db_table_name "$DB_TABLE_NAME" --db_name "$DB_NAME" --db_host_name "$DB_HOST_NAME" --db_port "$DB_PORT" --db_user_name "$DB_USER_NAME" --db_password "$DB_PASSWORD"
